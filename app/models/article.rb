30c61684f550fa099deea9c7cc25d82c10ffc340
hiroaz511 <hirokianzai511@gmail.com>
2018-10-30 00:28:51 +0900

    first commit

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,6 @@
 class Article < ApplicationRecord
   has_many :comments, dependent: :destroy
-  belongs_to :users
+  belongs_to :user
   validates :title, presence: true,
                     length: { minimum: 5 }
   validates :user_id, presence: true
