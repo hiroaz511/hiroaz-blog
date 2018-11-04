07469d6e845ba9eeca2de25509f6eec0d05212a5
hiroaz511 <hirokianzai511@gmail.com>
2018-11-04 22:10:16 +0900

    first commit

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -4,9 +4,13 @@ class LikesController < ApplicationController
  def create
  	@like=Like.new(user_id: @current_user.id, article_id: params[:article_id])
  	@like.save
- 	if request.path_info == '/articles'
+ 	puts "!!!!!!!!!!!!!!!!!!!!!!1" + request.path_info
+ 	puts "??????????????????????" + '/articles/#{article_id}/create'
+ 	if request.path_info == '/articles/#{article_id}/create'
+ 		puts "一覧ページにいます"
  		redirect_to("/articles")
  	else
+ 		puts "それ以外のページにいます"
  		redirect_to("/articles/#{params[:article_id]}")
 	end
  end
@@ -14,9 +18,12 @@ class LikesController < ApplicationController
  def destroy
  	@like = Like.find_by(user_id: @current_user.id, article_id: params[:article_id])
     @like.destroy
-    if request.path_info == '/articles'
+    puts "!!!!!!!!!!!!!!!!!!!!!!1" + request.path_info
+    if request.path_info == '/articles/#{@current_user.id}/create'
+    	puts "一覧ページにいます"
  		redirect_to("/articles")
  	else
+ 		puts "それ以外のページにいます"
  		redirect_to("/articles/#{params[:article_id]}")
 	end
  end
