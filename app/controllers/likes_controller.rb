b0e866d6d7b11b800012cb93b24bb556b21f80fa
hiroaz511 <hirokianzai511@gmail.com>
2018-11-08 14:27:51 +0900

    first commit

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,27 +1,39 @@
 class LikesController < ApplicationController
  before_action :authenticate_user
 
- def create
- 	@like=Like.new(user_id: @current_user.id, article_id: params[:article_id])
- 	@like.save
- 	puts "!!!!!!!!!!!!!!!!!!!!!!" + request.path_info
- 	puts "??????????????????????" + '/articles/#{article_id}/create'
+ # def create
+ # 	@like=Like.new(user_id: @current_user.id, article_id: params[:article_id])
+ # 	@like.save
+ 	# @articles.reload
+ 	# puts "!!!!!!!!!!!!!!!!!!!!!!" + request.path_info
+ 	# puts "??????????????????????" + '/articles/#{article_id}/create'
  # 	if request.path_info == '/articles/#{article_id}/create'
  # 		puts "一覧ページにいます"
- 		redirect_to("/articles")
+ 		# redirect_to("/articles")
  # 	else
  # 		puts "それ以外のページにいます"
- # 		redirect_to("/articles/#{params[:article_id]}")
+ 		# redirect_to("/articles/#{params[:article_id]}")
 	# end
- end
+ # end
+
+ def create
+ 	@like=Like.new(user_id: @current_user.id, article_id: params[:article_id])
+    @article = Article.find_by(id: params[:article_id])
+ 	@like.save
+
+end
+
 
  def destroy
  	@like = Like.find_by(user_id: @current_user.id, article_id: params[:article_id])
+    @article = Article.find_by(id: params[:article_id])
     @like.destroy
-    puts "!!!!!!!!!!!!!!!!!!!!!!1" + request.path_info
+
+    # @articles.reload
+    # puts "!!!!!!!!!!!!!!!!!!!!!!1" + request.path_info
  #    if request.path_info == '/articles/#{@current_user.id}/create'
  #    	puts "一覧ページにいます"
- 		redirect_to("/articles")
+ 		# redirect_to("/articles")
  # 	else
  # 		puts "それ以外のページにいます"
  # 		redirect_to("/articles/#{params[:article_id]}")
